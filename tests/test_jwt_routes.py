dff8ece5a61f790c584f34cdaefb428172b2e851
Joe Gasewicz <joegasewicz@gmail.com>
2019-12-06 13:59:29 +0000

    token #115

--- tests/test_jwt_routes.py
+++ tests/test_jwt_routes.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 from flask_jwt_router._jwt_routes import JwtRoutes
 from tests.fixtures.model_fixtures import MockEntityModel
+from tests.fixtures.token_fixture import mock_token
 
 
 class TestJwtRoutes:
@@ -10,7 +11,9 @@ class TestJwtRoutes:
     app = Flask(__name__)
     jwt_routes = JwtRoutes(app)
 
-    def test_create_token_from_model(self, MockEntityModel):
+    def test_create_token_from_model(self, MockEntityModel, mock_token):
         token = self.jwt_routes.create_token_from_model(MockEntityModel)
         assert isinstance(token, str)
+
+        assert token == mock_token
 
