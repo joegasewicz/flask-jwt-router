dc9f5705c4e463c2235a7d3ea70c94a992713a79
Joe Gasewicz <joegasewicz@gmail.com>
2019-12-11 15:23:43 +0000

    Pre-flight OPTIONS only set on POST #63
    **Release 0.0.26** - 2019-12-11
    
    - Preflight OPTIONS bug fix [Issue #125](https://github.com/joegasewicz/Flask-JWT-Router/issues/125)

--- flask_jwt_router/_routing.py
+++ flask_jwt_router/_routing.py
@@ -113,7 +113,7 @@ class Routing(BaseRouting):
         path = request.path
         for white_route in white_routes:
             if self._handle_pre_flight(method):
-                return False
+               return False
             if method == white_route[0] and path == white_route[1]:
                return False
             if method == white_route[0] and self._handle_query_params(white_route[1], path):
